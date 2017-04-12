eac6977cb84b56a6538e10af8d4ccca847905fe4
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2017-04-12 07:45:33 +0200

    add runWithoutError - improved modularity and error messages

--- src/main/scala/scalatest/ProgramMatcher.scala
+++ src/main/scala/scalatest/ProgramMatcher.scala
@@ -7,6 +7,7 @@ import org.scalatest._, matchers._, Matchers._
  */
 trait ProgramMatchers[P[_]]{
   def beSatisfied: Matcher[P[Boolean]]
+  def runWithoutErrors: Matcher[P[_]]
 }
 
 object ProgramMatchers{
@@ -16,19 +17,41 @@ object ProgramMatchers{
   trait Syntax{
     def beSatisfied[P[_]](implicit S1: ProgramMatchers[P]) =
       S1.beSatisfied
+    def runWithoutErrors[P[_]](implicit PM: ProgramMatchers[P]) = 
+      PM.runWithoutErrors
   }
 
   object Syntax extends Syntax
 
   implicit def matcher[P[_], E](implicit test: Tester[P,E]) =
     new ProgramMatchers[P]{
+      import scalaz.\/
+
       def beSatisfied = new Matcher[P[Boolean]]{
         def apply(program: P[Boolean]) = {
-          val evaluated = test(program)
+          val evaluated: E \/ Boolean = test(program)
           MatchResult(
             evaluated.fold(_ => false, identity),
-            evaluated.fold(_.toString, r => "should not happen"),
-            evaluated.fold(_.toString, r => "should not happen"))
+            evaluated.fold(
+              error => s"Unexpected error $error", 
+              _ => "Boolean program returned false"),
+            evaluated.fold(
+              _ => "should not happen", 
+              _ => s"Boolean program returned true"))
+        }
+      }
+
+      def runWithoutErrors = new Matcher[P[_]]{
+        def apply(program: P[_]) = {
+          val evaluated = test(program)
+          MatchResult(
+            evaluated.isRight,
+            evaluated.fold(
+              _.toString, 
+              _ => "should not happen"),
+            evaluated.fold(
+              _ => "should not happen", 
+              result => s"Unexpected returned value $result"))  
         }
       }
     }
--- src/main/scala/scalatest/StateProgramMatcher.scala
+++ src/main/scala/scalatest/StateProgramMatcher.scala
@@ -4,7 +4,7 @@ package scalatestImpl
 import org.scalatest._, matchers._, Matchers._
 
 trait ProgramStateMatchers[P[_],S]{
-  def beSatisfied(from: S): Matcher[P[Boolean]]
+  def apply(from: S): ProgramMatchers[P]
 }
 
 object ProgramStateMatchers{
@@ -13,14 +13,16 @@ object ProgramStateMatchers{
 
   trait Syntax{
     def beSatisfied[P[_],S](from: S)(implicit S1: ProgramStateMatchers[P,S]) =
-      S1.beSatisfied(from)
+      S1(from).beSatisfied
+    def runWithoutErrors[P[_],S](from: S)(implicit PM: ProgramStateMatchers[P,S]) = 
+      PM(from).runWithoutErrors
   }
 
   object Syntax extends Syntax
 
   implicit def matcher[P[_], S, E](implicit tester: StateTester[P,S,E]) =
     new ProgramStateMatchers[P,S]{
-      def beSatisfied(from: S) = 
-        ProgramMatchers.matcher(tester(from)).beSatisfied
+      def apply(from: S) = 
+        ProgramMatchers.matcher(tester(from))
     }
 }
